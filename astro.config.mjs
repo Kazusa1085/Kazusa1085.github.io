101897984fc9a8e764e368143f02db4feeb2e568
Cathy Palug <195487617+Kazusa1085@users.noreply.github.com>
2025-04-19 03:11:14 +0800

    Update astro.config.mjs
    Set My Domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: 'https://Kazusa1085.github.io',
+  base: 'my-repo',
   trailingSlash: "always",
   integrations: [
     tailwind(
